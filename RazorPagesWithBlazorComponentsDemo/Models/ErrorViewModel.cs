f3502f05fe9d833d3f33124839f694cef5ff23ca
markm <markm@MA5479648>
2024-03-07 13:54:02 -0500

    Add project files.

---
+++ RazorPagesWithBlazorComponentsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RazorPagesWithBlazorComponentsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
